ad5428cb7824042c7da10a2ac8ef5b8f953402d6
“tomh543” <tomh543@hotmail.co.uk>
2022-04-16 22:26:40 +0100

    Update ec2tos3.py

--- code/ec2tos3.py
+++ code/ec2tos3.py
@@ -5,17 +5,17 @@ import time
 #using put object function to upload images to Bucket coursework-bucket-s1311448
 #use time.sleep(30) function in order to put a 30second delay between file uploads.
 cli = boto3.client('s3')
-cli.put_object(Bucket='coursework-bucket-s1311448', Key='/CPDimages/images/image1.jpg')
+cli.put_object(Bucket='coursework-bucket-s1311448', Key='CPDimages/images/image1.jpg')
 time.sleep(30),
 cli = boto3.client('s3')
-cli.put_object(Bucket='coursework-bucket-s1311448', Key='/CPDimages/images/image2.jpg')
+cli.put_object(Bucket='coursework-bucket-s1311448', Key='CPDimages/images/image2.png')
 time.sleep(30),
 cli = boto3.client('s3')
-cli.put_object(Bucket='coursework-bucket-s1311448', Key='/CPDimages/images/image3.jpg')
+cli.put_object(Bucket='coursework-bucket-s1311448', Key='CPDimages/images/image3.jpg')
 time.sleep(30),
 cli = boto3.client('s3')
-cli.put_object(Bucket='coursework-bucket-s1311448', Key='/CPDimages/images/image4.jpg')
+cli.put_object(Bucket='coursework-bucket-s1311448', Key='CPDimages/images/image4.jpg')
 time.sleep(30),
 cli = boto3.client('s3')
-cli.put_object(Bucket='coursework-bucket-s1311448', Key='/CPDimages/images/image5.jpg')
+cli.put_object(Bucket='coursework-bucket-s1311448', Key='CPDimages/images/image5.jpg')
 time.sleep(30)
