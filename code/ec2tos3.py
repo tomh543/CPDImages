8cdadd4853c7b3fd4c3825a3a26e91652a613401
“tomh543” <tomh543@hotmail.co.uk>
2022-04-16 22:50:07 +0100

    Update ec2tos3.py

--- code/ec2tos3.py
+++ code/ec2tos3.py
@@ -4,6 +4,5 @@ import boto3
 import time
 #using put object function to upload images to Bucket coursework-bucket-s1311448
 #use time.sleep(30) function in order to put a 30second delay between file uploads.
-cli = boto3.client('s3')
-s3.bucket('coursework-bucket-s1311448').object('images').upload_file('CPDImages/images')
-time.sleep(30)
+s3 = boto3.resource('s3')
+s3.meta.client.upload_file('CPDImages/images/image1.jpg', 'coursework-bucket-s1311448', 'image1.jpg')
