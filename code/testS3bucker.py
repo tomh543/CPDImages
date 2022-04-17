96837419f0a9c3aa4f2f1bedd07aa36c73696d12
“tomh543” <tomh543@hotmail.co.uk>
2022-04-17 06:16:44 +0100

    Update testS3bucker.py

--- code/testS3bucker.py
+++ code/testS3bucker.py
@@ -1,8 +1,15 @@
-    # Create bucket
+    #import of boto3 in order to run the python code
+    import boto3
+    #import of logging & ClientError in order to allow for error testing
+    import logging
+    from botocore.exceptions import ClientError
+    # function to create bucket
+    def create_bucket(bucket_name, region=None):
+    #creating the bucket if bucket created it will return True else false, if region is not specified it will default to us-east-1
     try:
-        if region is 'us-east-1":
+        if region is None:
             s3_client = boto3.client('s3')
-            s3_client.create_bucket(Bucket='testbettercode')
+            s3_client.create_bucket(Bucket='bucket_name')
         else:
             s3_client = boto3.client('s3', region_name=region)
             location = {'LocationConstraint': region}
@@ -12,3 +19,7 @@
         logging.error(e)
         return False
     return True
+    
+    #running the function to create the bucket
+    #creates bucket called testbettercode
+    create_bucket("testbettercode", "us-east-1")
