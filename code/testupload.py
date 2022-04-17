3f48916e84983062baef77207eab3a2cb36c702a
“tomh543” <tomh543@hotmail.co.uk>
2022-04-17 07:49:52 +0100

    Update testupload.py

--- code/testupload.py
+++ code/testupload.py
@@ -1,5 +1,24 @@
+import glob
 import boto3
-def upload_to_s3():
-    s3 = boto3.resource('s3')
-    s3.meta.client.upload_file('CPDImages/images', 'coursework-bucket-s1311448', 'image1.jpg' --recursive)
-upload_to_s3()
+import os
+
+BUCKET_NAME = 'coursework-bucket-s1311448'
+FOLDER_NAME = 'CPDImages/images/'
+
+session = boto3.Session(profile_name='default')
+s3 = session.client('s3')
+
+jpg_files = glob.glob("CPDImages/images/*.jpg")
+png_files = glob.glob("CPDImages/images/*.jpg.png")
+
+for filename in jpg_files:
+    key = "%s/%s" % (FOLDER_NAME, os.path.basename(filename))
+    print("Putting %s as %s" % (filename,key))
+    s3.upload_file(filename, BUCKET_NAME, key)
+
+for filename in png_files:
+    key = "%s/%s" % (FOLDER_NAME, os.path.basename(filename))
+    print("Putting %s as %s" % (filename,key))
+    s3.upload_file(filename, BUCKET_NAME, key)
+
+print("All_Done")
