be2760e0f6d3e4a5e92d2a69f2ce5d7f58be94de
“tomh543” <tomh543@hotmail.co.uk>
2022-04-17 07:44:02 +0100

    Update testupload.py

--- code/testupload.py
+++ code/testupload.py
@@ -1,4 +1,4 @@
-def upload_to_s3(df):
+def upload_to_s3():
     s3 = boto3.resource('s3')
     s3.meta.client.upload_file('CPDImages/images', 'coursework-bucket-s1311448', 'image1.jpg' --recursive)
-upload_to_s3(df)
+upload_to_s3()
