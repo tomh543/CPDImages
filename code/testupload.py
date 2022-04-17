4ff2cddee568694dd506eaaf3e81dae7369302f2
“tomh543” <tomh543@hotmail.co.uk>
2022-04-17 07:52:04 +0100

    Update testupload.py

--- code/testupload.py
+++ code/testupload.py
@@ -12,11 +12,11 @@ png_files = glob.glob("CPDImages/images/*.jpg.png")
 for filename in jpg_files:
     key = "%s/%s" % (FOLDER_NAME, os.path.basename(filename))
     print("Putting %s as %s" % (filename,key))
-    s3.upload_file(filename, BUCKET_NAME, key)
+    s3.meta.client.upload_file(filename, BUCKET_NAME, key)
 
 for filename in png_files:
     key = "%s/%s" % (FOLDER_NAME, os.path.basename(filename))
     print("Putting %s as %s" % (filename,key))
-    s3.upload_file(filename, BUCKET_NAME, key)
+    s3.meta.client.upload_file(filename, BUCKET_NAME, key)
 
 print("All_Done")
